bdce4ff3984bafdcab33fd506be261d9068eb607
shakuu <todorov.h@gmail.com>
2017-02-23 00:54:08 +0200

    added CommandParsingStrategy

---
+++ Parser/Src/Parser.FileReader/Strategies/CommandParsingStrategy.cs
@@ -0,0 +1,13 @@
+﻿using Parser.FileReader.Contracts;
+
+namespace Parser.FileReader.Strategies
+{
+    public class CommandParsingStrategy : ICommandParsingStrategy
+    {
+        public ICommand ParseInputCommand(string input)
+        {
+            // TODO: 
+            return null;
+        }
+    }
+}
