7fe66e6786c8bb1454f5524afad7190b4504f04e
shakuu <todorov.h@gmail.com>
2017-02-24 02:48:35 +0200

    added wpf client

---
+++ Parser/Tests/Parser.FileReader.Tests/EnginesTests/FileReaderEngineTests/Start_Should.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Moq;
+using NUnit.Framework;
+
+using Parser.FileReader.Contracts;
+using Parser.FileReader.Engines;
+using Parser.FileReader.Factories;
+
+namespace Parser.FileReader.Tests.EnginesTests.FileReaderEngineTests
+{
+    [TestFixture]
+    public class Start_Should
+    {
+        [TestCase(null)]
+        [TestCase("")]
+        public void ThrowArgumentNullException_WhenLogFilePathParameterIsNullOrEmpty(string invalidLogFilePath)
+        {
+            // Arrange
+            var commandParsingStrategy = new Mock<ICommandParsingStrategy>();
+            var commandUtilizationStrategy = new Mock<ICommandUtilizationStrategy>();
+            var fileReaderAutoResetEventFactory = new Mock<IFileReaderAutoResetEventFactory>();
+            var fileReaderFileSystemWatcherFactory = new Mock<IFileReaderFileSystemWatcherFactory>();
+            var fileReaderInputProviderFactory = new Mock<IFileReaderInputProviderFactory>();
+
+            var actualInstance = new FileReaderEngine(commandParsingStrategy.Object, commandUtilizationStrategy.Object, fileReaderAutoResetEventFactory.Object, fileReaderFileSystemWatcherFactory.Object, fileReaderInputProviderFactory.Object);
+
+            // Act & Assert
+            Assert.That(
+                () => actualInstance.Start(invalidLogFilePath),
+                Throws.InstanceOf<ArgumentNullException>().With.Message.Contains("Invalid log file path."));
+        }
+    }
+}
