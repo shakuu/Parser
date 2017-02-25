fb0175288ed4c9b9b3414e4cba3e39971d421426
shakuu <todorov.h@gmail.com>
2017-02-25 03:26:20 +0200

    added IEnvironmentFolderPathProvider as LogFilePathDiscoveryStrategy ctor param

--- Parser/Src/Parser.LogFileReader/Strategies/LogFilePathDiscoveryStrategy.cs
+++ Parser/Src/Parser.LogFileReader/Strategies/LogFilePathDiscoveryStrategy.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
+
+using Bytes2you.Validation;
 
 using Parser.LogFileReader.Contracts;
 
@@ -8,11 +9,21 @@ namespace Parser.LogFileReader.Strategies
 {
     public class LogFilePathDiscoveryStrategy : ILogFilePathDiscoveryStrategy
     {
+        private const string MyDocumentsName = "MyDocuments";
         private const string SwtorDefaultPath = "\\Star Wars - The Old Republic\\CombatLogs";
+
+        private readonly IEnvironmentFolderPathProvider environmentFolderPathProvider;
+
+        public LogFilePathDiscoveryStrategy(IEnvironmentFolderPathProvider environmentFolderPathProvider)
+        {
+            Guard.WhenArgument(environmentFolderPathProvider, nameof(IEnvironmentFolderPathProvider)).IsNull().Throw();
+
+            this.environmentFolderPathProvider = environmentFolderPathProvider;
+        }
 
         public string DiscoverLogFile()
         {
-            var documentsFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var documentsFolderPath = this.environmentFolderPathProvider.GetEnvironmentFolderPath(LogFilePathDiscoveryStrategy.MyDocumentsName);
             var combatLogsPath = documentsFolderPath + LogFilePathDiscoveryStrategy.SwtorDefaultPath;
             var logFilePath = Directory.GetFiles(combatLogsPath);
 
