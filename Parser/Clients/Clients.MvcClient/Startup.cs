180bb769fa4d42dcde77d993eae9b8c17b2b3dc5
shakuu <todorov.h@gmail.com>
2017-02-26 14:23:31 +0200

    added MVC Client

---
+++ Parser/Clients/Clients.MvcClient/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Clients.MvcClient.Startup))]
+namespace Clients.MvcClient
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+
+            ConfigureAuth(app);
+        }
+    }
+}
