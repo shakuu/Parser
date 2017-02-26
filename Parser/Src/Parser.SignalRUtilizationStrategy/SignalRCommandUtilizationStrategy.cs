bca67dca470e3ec092d2bf03f61487986949eccc
shakuu <todorov.h@gmail.com>
2017-02-26 18:28:26 +0200

    added IJsonConvertProvider param to SignalRCommandUtilizationStrategy ctor

--- Parser/Src/Parser.SignalRUtilizationStrategy/SignalRCommandUtilizationStrategy.cs
+++ Parser/Src/Parser.SignalRUtilizationStrategy/SignalRCommandUtilizationStrategy.cs
@@ -3,25 +3,34 @@ using System.Threading.Tasks;
 
 using Microsoft.AspNet.SignalR.Client;
 
+using Bytes2you.Validation;
+
 using Newtonsoft.Json;
 
 using Parser.LogFileReader.Contracts;
+using Parser.SignalRUtilizationStrategy.Contracts;
 
 namespace Parser.SignalRUtilizationStrategy
 {
     public class SignalRCommandUtilizationStrategy : ICommandUtilizationStrategy
     {
+        private readonly IJsonConvertProvider jsonConvertProvider;
+
         private readonly IHubProxy logFileParserHubProxy;
         private string assignedId;
 
-        public SignalRCommandUtilizationStrategy()
+        public SignalRCommandUtilizationStrategy(IJsonConvertProvider jsonConvertProvider)
         {
+            Guard.WhenArgument(jsonConvertProvider, nameof(IJsonConvertProvider)).IsNull().Throw();
+
+            this.jsonConvertProvider = jsonConvertProvider;
+
             this.logFileParserHubProxy = this.CreateProxy("http://localhost:52589").Result;
         }
 
         public void UtilizeCommand(ICommand command)
         {
-            var serializedCommand = JsonConvert.SerializeObject(command);
+            var serializedCommand = this.jsonConvertProvider.SerializeObject(command);
 
             this.logFileParserHubProxy.Invoke("SendCommand", this.assignedId, serializedCommand);
         }
