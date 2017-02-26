92a29b78356dfa17a379fda05451f4b2622592dd
shakuu <todorov.h@gmail.com>
2017-02-26 18:23:33 +0200

    added IJsonConvertProvider

---
+++ Parser/Src/Parser.SignalRUtilizationStrategy/Providers/JsonConvertProvider.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+using Parser.SignalRUtilizationStrategy.Contracts;
+
+namespace Parser.SignalRUtilizationStrategy.Providers
+{
+    public class JsonConvertProvider : IJsonConvertProvider
+    {
+        public string SerializeObject(object value)
+        {
+            return JsonConvert.SerializeObject(value);
+        }
+    }
+}
