953d265653680370e4de72d7fbcb977cf1a8017a
shakuu <todorov.h@gmail.com>
2017-03-01 01:19:39 +0200

    scaffolded LogFileParserEngineManager and LogFileParserEngine

---
+++ Parser/Src/Parser.LogFileParser/Engines/LogFileParserEngine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Parser.Common.Contracts;
+using Parser.LogFileParser.Contracts;
+
+namespace Parser.LogFileParser.Engines
+{
+    public class LogFileParserEngine : ILogFileParserEngine
+    {
+        public void EnqueueCommand(ICommand command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IParseResult GetParseResult()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Parser/Src/Parser.LogFileParser/Managers/LogFileParserEngineManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Parser.Common.Contracts;
+using Parser.LogFileParser.Contracts;
+
+namespace Parser.LogFileParser.Managers
+{
+    public class LogFileParserEngineManager : ILogFileParserEngineManager
+    {
+        public string EnqueueCommandToEngineWithId(string engineId, ICommand command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string StartNewLogFileParserEngine()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
