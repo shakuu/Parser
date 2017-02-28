daa595319997737241a5b19423e0968dbfff0da9
shakuu <todorov.h@gmail.com>
2017-03-01 01:43:30 +0200

    added NinjectModule-s to MvcClient

---
+++ Parser/Clients/Parser.MvcClient/App_Start/NinjectModules/LogFileParserNinjectModule.cs
@@ -0,0 +1,33 @@
+﻿using Ninject.Extensions.Conventions;
+using Ninject.Extensions.Conventions.Syntax;
+using Ninject.Modules;
+
+namespace Parser.MvcClient.App_Start.NinjectModules
+{
+    public class LogFileParserNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Bind(this.BindAllClassesByConvention);
+            this.Bind(this.BindFactoriesByConvention);
+        }
+
+        private void BindAllClassesByConvention(IFromSyntax bind)
+        {
+            bind
+                .FromAssembliesMatching("*.LogFileParser.*")
+                .SelectAllClasses()
+                .BindDefaultInterface();
+        }
+
+        private void BindFactoriesByConvention(IFromSyntax bind)
+        {
+            bind
+                .FromAssembliesMatching("*.LogFileParser.*")
+                .SelectAllInterfaces()
+                .EndingWith("Factory")
+                .BindToFactory()
+                .Configure(f => f.InSingletonScope());
+        }
+    }
+}
