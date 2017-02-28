089319a8de54c34e2a7e938d55619ed665739c05
shakuu <todorov.h@gmail.com>
2017-03-01 01:47:39 +0200

    added basic LogFileParserEngine implementation

--- Parser/Src/Parser.LogFileParser/Engines/LogFileParserEngine.cs
+++ Parser/Src/Parser.LogFileParser/Engines/LogFileParserEngine.cs
@@ -1,20 +1,41 @@
-﻿using System;
+﻿using System.Collections.Generic;
+
+using Bytes2you.Validation;
 
 using Parser.Common.Contracts;
+using Parser.Common.Factories;
 using Parser.LogFileParser.Contracts;
 
 namespace Parser.LogFileParser.Engines
 {
     public class LogFileParserEngine : ILogFileParserEngine
     {
+        private readonly IParseResultFactory parseResultFactory;
+
+        private readonly Queue<ICommand> commandsQueue;
+
+        public LogFileParserEngine(IParseResultFactory parseResultFactory)
+        {
+            Guard.WhenArgument(parseResultFactory, nameof(IParseResultFactory)).IsNull().Throw();
+
+            this.parseResultFactory = parseResultFactory;
+
+            this.commandsQueue = new Queue<ICommand>();
+
+            // TODO: Async parsing
+        }
+
         public void EnqueueCommand(ICommand command)
         {
-            throw new NotImplementedException();
+            Guard.WhenArgument(command, nameof(ICommand)).IsNull().Throw();
+
+            this.commandsQueue.Enqueue(command);
         }
 
         public IParseResult GetParseResult()
         {
-            throw new NotImplementedException();
+            // TODO: 
+            return this.parseResultFactory.CreateParseResult();
         }
     }
 }
