2099c0d201b3c40864bbfbbf6cd145970dafd5fb
shakuu <todorov.h@gmail.com>
2017-03-01 22:37:45 +0200

    using lower case urls

--- Parser/Clients/Parser.MvcClient/App_Start/RouteConfig.cs
+++ Parser/Clients/Parser.MvcClient/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace Parser.MvcClient
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.LowercaseUrls = true;
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
