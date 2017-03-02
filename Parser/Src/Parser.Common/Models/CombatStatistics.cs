637a267e7497a772a3328a116ca01cbcaf943eee
shakuu <todorov.h@gmail.com>
2017-03-02 11:22:18 +0200

    added EnterCombat and ExitCombat props to ICombatStatistics

--- Parser/Src/Parser.Common/Models/CombatStatistics.cs
+++ Parser/Src/Parser.Common/Models/CombatStatistics.cs
@@ -1,9 +1,15 @@
-﻿using Parser.Common.Contracts;
+﻿using System;
+
+using Parser.Common.Contracts;
 
 namespace Parser.Common.Models
 {
     public class CombatStatistics : ICombatStatistics
     {
+        public DateTime EnterCombatTime { get; set; }
+
+        public DateTime ExitCombatTime { get; set; }
+
         public decimal DamageDone { get; set; }
 
         public decimal HealingDone { get; set; }
