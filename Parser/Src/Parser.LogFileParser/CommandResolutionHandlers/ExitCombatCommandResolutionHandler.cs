81dd9fd506a172eaad6adee97d7e8200da47fc02
shakuu <todorov.h@gmail.com>
2017-03-02 16:25:01 +0200

    added ExitCombatCommandResolutionHandler

---
+++ Parser/Src/Parser.LogFileParser/CommandResolutionHandlers/ExitCombatCommandResolutionHandler.cs
@@ -0,0 +1,24 @@
+﻿using Parser.Common.Contracts;
+using Parser.LogFileParser.CommandResolutionHandlers.Base;
+using Parser.LogFileParser.Contracts;
+
+namespace Parser.LogFileParser.CommandResolutionHandlers
+{
+    public class ExitCombatCommandResolutionHandler : CommandResolutionHandler, ICommandResolutionHandler
+    {
+        private const string ViableEventName = "ExitCombat";
+
+        protected override bool CanHandleCommand(ICommand command)
+        {
+            return command.EventName == ExitCombatCommandResolutionHandler.ViableEventName;
+        }
+
+        protected override ICombatStatisticsContainer HandleCommand(ICommand command, ICombatStatisticsContainer combatStatisticsContainer)
+        {
+            combatStatisticsContainer.CurrentComabtStatistics.IsCompleted = true;
+            combatStatisticsContainer.CurrentComabtStatistics = null;
+
+            return combatStatisticsContainer;
+        }
+    }
+}
