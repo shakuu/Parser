5eb40cd42dce1ab3aa7c33a10558077548f4f7d8
shakuu <todorov.h@gmail.com>
2017-03-03 12:08:54 +0200

    added Parser.Common.Tests

---
+++ Parser/Tests/Parser.Common.Tests/ProvidersTests/CommandJsonConvertProviderTests/Constructor_Should.cs
@@ -0,0 +1,10 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace Parser.Common.Tests.ProvidersTests.CommandJsonConvertProviderTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+    }
+}
