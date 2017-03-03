a936a49adf3dc19708a72647ea88198e51fb10ce
shakuu <todorov.h@gmail.com>
2017-03-03 10:32:46 +0200

    added LogFileParserHubService tests

---
+++ Parser/Tests/Parser.SignalR.Tests/ServicesTests/LogFileParserHubServiceTests/Constructor_Should.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using Moq;
+using NUnit.Framework;
+
+using Parser.LogFileParser.Contracts;
+using Parser.SignalR.Contracts;
+using Parser.SignalR.Services;
+
+namespace Parser.SignalR.Tests.ServicesTests.LogFileParserHubServiceTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void CreateCorrectILogFileParserHubServiceInstance_WhenParametersAreValid()
+        {
+            // Arrange
+            var logFileParserEngineManager = new Mock<ILogFileParserEngineManager>();
+            var jsonConvertProvider = new Mock<IJsonConvertProvider>();
+
+            // Act
+            var actualInstance = new LogFileParserHubService(logFileParserEngineManager.Object, jsonConvertProvider.Object);
+
+            // Assert
+            Assert.That(actualInstance, Is.Not.Null.And.InstanceOf<ILogFileParserHubService>());
+        }
+
+        [Test]
+        public void ThrowArgumentNullException_WhenILogFileParserEngineManagerParameterIsNull()
+        {
+            // Arrange
+            ILogFileParserEngineManager logFileParserEngineManager = null;
+            var jsonConvertProvider = new Mock<IJsonConvertProvider>();
+
+            // Act & Assert
+            Assert.That(
+                () => new LogFileParserHubService(logFileParserEngineManager, jsonConvertProvider.Object),
+                Throws.InstanceOf<ArgumentNullException>().With.Message.Contains(nameof(ILogFileParserEngineManager)));
+        }
+
+        [Test]
+        public void ThrowArgumentNullException_WhenIJsonConvertProviderParameterIsNull()
+        {
+            // Arrange
+            var logFileParserEngineManager = new Mock<ILogFileParserEngineManager>();
+            IJsonConvertProvider jsonConvertProvider = null;
+
+            // Act & Assert
+            Assert.That(
+                () => new LogFileParserHubService(logFileParserEngineManager.Object, jsonConvertProvider),
+                Throws.InstanceOf<ArgumentNullException>().With.Message.Contains(nameof(IJsonConvertProvider)));
+        }
+    }
+}
---
+++ Parser/Tests/Parser.SignalR.Tests/ServicesTests/LogFileParserHubServiceTests/GetParsingSessionId_Should.cs
@@ -0,0 +1,48 @@
+﻿using Moq;
+using NUnit.Framework;
+
+using Parser.LogFileParser.Contracts;
+using Parser.SignalR.Contracts;
+using Parser.SignalR.Services;
+
+namespace Parser.SignalR.Tests.ServicesTests.LogFileParserHubServiceTests
+{
+    [TestFixture]
+    public class GetParsingSessionId_Should
+    {
+        [Test]
+        public void ShouldInvokeILogFileParserEngineManager_StartNewLogFileParserEngineMethodOnce()
+        {
+            // Arrange
+            var logFileParserEngineManager = new Mock<ILogFileParserEngineManager>();
+            var jsonConvertProvider = new Mock<IJsonConvertProvider>();
+
+            var logFileParserHubService = new LogFileParserHubService(logFileParserEngineManager.Object, jsonConvertProvider.Object);
+
+            // Act
+            logFileParserHubService.GetParsingSessionId();
+
+            // Assert
+            logFileParserEngineManager.Verify(m => m.StartNewLogFileParserEngine(), Times.Once);
+        }
+
+        [Test]
+        public void ShouldReturnCorrectStringValue()
+        {
+            // Arrange
+            var logFileParserEngineManager = new Mock<ILogFileParserEngineManager>();
+            var jsonConvertProvider = new Mock<IJsonConvertProvider>();
+
+            var logFileParserHubService = new LogFileParserHubService(logFileParserEngineManager.Object, jsonConvertProvider.Object);
+
+            var expectedReturnValue = "expected return value";
+            logFileParserEngineManager.Setup(m => m.StartNewLogFileParserEngine()).Returns(expectedReturnValue);
+
+            // Act
+            var actualReturnValue = logFileParserHubService.GetParsingSessionId();
+
+            // Assert
+            Assert.That(actualReturnValue, Is.EqualTo(expectedReturnValue));
+        }
+    }
+}
