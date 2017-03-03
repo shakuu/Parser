777d575b794e9c4a8033e495f43742684d412451
shakuu <todorov.h@gmail.com>
2017-03-03 02:48:43 +0200

    refactoring

--- Parser/Src/Parser.SignalR/Contracts/ILogFileParserHubService.cs
+++ Parser/Src/Parser.SignalR/Contracts/ILogFileParserHubService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Parser.SignalR.Contracts
+﻿namespace Parser.SignalR.Contracts
 {
     public interface ILogFileParserHubService
     {
