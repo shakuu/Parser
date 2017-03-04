d7b62e10b74e8c034cc42472770c5bfb611b33f9
shakuu <todorov.h@gmail.com>
2017-03-04 12:57:38 +0200

    added MockEnterCombatCommandResolutionHandler

---
+++ Parser/Tests/Parser.LogFileParser.Tests/CommandResolutionHandlersTests/EnterCombatCommandResolutionHandlerTests/CanHandleCommand_Should.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using NUnit.Framework;
+using Parser.Common.Contracts;
+using Parser.Common.Factories;
+using Parser.LogFileParser.CommandResolutionHandlers;
+
+namespace Parser.LogFileParser.Tests.CommandResolutionHandlersTests.EnterCombatCommandResolutionHandlerTests
+{
+    [TestFixture]
+    public class CanHandleCommand_Should
+    {
+        [TestCase("")]
+        [TestCase(null)]
+        public void ReturnFalse_WhenICommandObjectEventNamePropertyIsANullOrEmptyString(string eventName)
+        {
+            // Arrange
+            var combatStatisticsFactory = new Mock<ICombatStatisticsFactory>();
+
+            var enterCombatCommandResolutionHandler = new EnterCombatCommandResolutionHandler(combatStatisticsFactory.Object);
+
+            var command = new Mock<ICommand>();
+            command.SetupGet(c => c.EventName).Returns(eventName);
+
+            // Act 
+            //var actualResult = enterCombatCommandResolutionHandler.
+        }
+    }
+}
