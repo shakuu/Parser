18cfc111a1a96308639f9989037e91358c432030
shakuu <todorov.h@gmail.com>
2017-03-04 17:37:25 +0200

    added ExitCombatCommandResolutionHandler ctor tests

---
+++ Parser/Tests/Parser.LogFileParser.Tests/CommandResolutionHandlersTests/ExitCombatCommandResolutionHandlerTests/HandleCommand_Should.cs
@@ -0,0 +1,9 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace Parser.LogFileParser.Tests.CommandResolutionHandlersTests.ExitCombatCommandResolutionHandlerTests
+{
+    public class HandleCommand_Should
+    {
+    }
+}
