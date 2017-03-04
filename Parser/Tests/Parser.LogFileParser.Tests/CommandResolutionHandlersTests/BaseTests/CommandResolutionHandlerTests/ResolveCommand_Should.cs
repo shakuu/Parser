c331140d3329dc4f459822d124be3a14fd4fd845
shakuu <todorov.h@gmail.com>
2017-03-04 03:04:04 +0200

    added MockCommandResolutionHandler

---
+++ Parser/Tests/Parser.LogFileParser.Tests/CommandResolutionHandlersTests/BaseTests/CommandResolutionHandlerTests/ResolveCommand_Should.cs
@@ -0,0 +1,10 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace Parser.LogFileParser.Tests.CommandResolutionHandlersTests.BaseTests.CommandResolutionHandlerTests
+{
+    [TestFixture]
+    public class ResolveCommand_Should
+    {
+    }
+}
---
+++ Parser/Tests/Parser.LogFileParser.Tests/Mocks/MockCommandResolutionHandler.cs
@@ -0,0 +1,26 @@
+﻿using Parser.Common.Contracts;
+using Parser.LogFileParser.CommandResolutionHandlers.Base;
+
+namespace Parser.LogFileParser.Tests.Mocks
+{
+    internal class MockCommandResolutionHandler : CommandResolutionHandler
+    {
+        public bool CanHandleCommandMethodIsCalled { get; set; }
+
+        internal bool HandleCommandMethodIsCalled { get; set; }
+
+        protected override bool CanHandleCommand(ICommand command)
+        {
+            this.CanHandleCommandMethodIsCalled = true;
+
+            return true;
+        }
+
+        protected override ICombatStatisticsContainer HandleCommand(ICommand command, ICombatStatisticsContainer combatStatisticsContainer)
+        {
+            this.HandleCommandMethodIsCalled = true;
+
+            return null;
+        }
+    }
+}
