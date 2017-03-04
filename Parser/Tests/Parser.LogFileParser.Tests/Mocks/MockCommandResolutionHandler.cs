c9f1a0d6b530a9192b79b5db29e45e6764289fef
shakuu <todorov.h@gmail.com>
2017-03-04 03:10:04 +0200

    refactoring MockCommandResolutionHandler

--- Parser/Tests/Parser.LogFileParser.Tests/Mocks/MockCommandResolutionHandler.cs
+++ Parser/Tests/Parser.LogFileParser.Tests/Mocks/MockCommandResolutionHandler.cs
@@ -5,22 +5,26 @@ namespace Parser.LogFileParser.Tests.Mocks
 {
     internal class MockCommandResolutionHandler : CommandResolutionHandler
     {
-        public bool CanHandleCommandMethodIsCalled { get; set; }
+        internal bool CanHandleCommandMethodIsCalled { get; set; }
+
+        internal bool CanHandleCommandMethodReturnValue { get; set; }
 
         internal bool HandleCommandMethodIsCalled { get; set; }
+
+        internal ICombatStatisticsContainer HandleCommandMethodReturnValue { get; set; }
 
         protected override bool CanHandleCommand(ICommand command)
         {
             this.CanHandleCommandMethodIsCalled = true;
 
-            return true;
+            return this.CanHandleCommandMethodReturnValue;
         }
 
         protected override ICombatStatisticsContainer HandleCommand(ICommand command, ICombatStatisticsContainer combatStatisticsContainer)
         {
             this.HandleCommandMethodIsCalled = true;
 
-            return null;
+            return this.HandleCommandMethodReturnValue;
         }
     }
 }
