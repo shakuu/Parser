defcf32f415d4c1ea60f10675e46d59d9062d067
shakuu <todorov.h@gmail.com>
2017-03-04 17:28:48 +0200

    refactoring ExitCombatCommandResolutionHandler to remove IDateTimeProvider

--- Parser/Src/Parser.LogFileParser/CommandResolutionHandlers/ExitCombatCommandResolutionHandler.cs
+++ Parser/Src/Parser.LogFileParser/CommandResolutionHandlers/ExitCombatCommandResolutionHandler.cs
@@ -1,23 +1,12 @@
-﻿using Bytes2you.Validation;
-
-using Parser.Common.Contracts;
+﻿using Parser.Common.Contracts;
 using Parser.LogFileParser.CommandResolutionHandlers.Base;
 using Parser.LogFileParser.Contracts;
 
 namespace Parser.LogFileParser.CommandResolutionHandlers
 {
-    public class ExitCombatCommandResolutionHandler : CommandResolutionHandler, ICommandResolutionHandler
+    public class ExitCombatCommandResolutionHandler : CommandResolutionHandler, ICommandResolutionHandler, ICommandResolutionHandlerChain
     {
         private const string ViableEventName = "ExitCombat";
-
-        private readonly IDateTimeProvider dateTimeProvider;
-
-        public ExitCombatCommandResolutionHandler(IDateTimeProvider dateTimeProvider)
-        {
-            Guard.WhenArgument(dateTimeProvider, nameof(IDateTimeProvider)).IsNull().Throw();
-
-            this.dateTimeProvider = dateTimeProvider;
-        }
 
         protected override bool CanHandleCommand(ICommand command)
         {
