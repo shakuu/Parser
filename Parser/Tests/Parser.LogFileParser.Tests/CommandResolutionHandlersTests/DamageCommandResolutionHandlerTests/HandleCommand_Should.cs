f4c8b96f2052f9451fb846f7afbc761f3b3de2ff
shakuu <todorov.h@gmail.com>
2017-03-06 01:44:30 +0200

    added DamageCommandResolutionHandler ctor tests

---
+++ Parser/Tests/Parser.LogFileParser.Tests/CommandResolutionHandlersTests/DamageCommandResolutionHandlerTests/HandleCommand_Should.cs
@@ -0,0 +1,16 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace Parser.LogFileParser.Tests.CommandResolutionHandlersTests.DamageCommandResolutionHandlerTests
+{
+    [TestFixture]
+    public class HandleCommand_Should
+    {
+        [Test]
+        public void IncrementICombatStatisticsContainerParameter_CurrentCombatStatisticsDamageDonePropertyWithCorrectAmount()
+        {
+            // Arrange
+
+        }
+    }
+}
