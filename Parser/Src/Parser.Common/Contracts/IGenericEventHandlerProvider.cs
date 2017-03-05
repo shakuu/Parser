649c81a14b1e776594e29d5089fc7a9efc74575e
shakuu <todorov.h@gmail.com>
2017-03-05 12:18:27 +0200

    refactoring GenericEventHandlerProvider

--- Parser/Src/Parser.Common/Contracts/IGenericEventHandlerProvider.cs
+++ Parser/Src/Parser.Common/Contracts/IGenericEventHandlerProvider.cs
@@ -5,10 +5,10 @@ namespace Parser.Common.Contracts
     public interface IGenericEventHandlerProvider<EventArgsType>
         where EventArgsType : EventArgs
     {
-        void Subscribe(Action<object, EventArgsType> action);
+        void Subscribe(EventHandler<EventArgsType> action);
 
-        void Unsubscribe(Action<object, EventArgsType> action);
+        void Unsubscribe(EventHandler<EventArgsType> action);
 
-        void Raise();
+        void Raise(EventArgsType eventArgs);
     }
 }
---
+++ Parser/Src/Parser.Common/Providers/GenericEventHandlerProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Parser.Common.Contracts;
+
+namespace Parser.Common.Providers
+{
+    public class GenericEventHandlerProvider<EventArgsType> : IGenericEventHandlerProvider<EventArgsType>
+        where EventArgsType : EventArgs
+    {
+        private event EventHandler<EventArgsType> onEvent;
+
+        protected virtual event EventHandler<EventArgsType> OnEvent
+        {
+            add
+            {
+                this.onEvent += value;
+            }
+
+            remove
+            {
+                this.onEvent -= value;
+            }
+        }
+
+        public virtual void Raise(EventArgsType eventArgs)
+        {
+            this.onEvent?.Invoke(this, eventArgs);
+        }
+
+        public virtual void Subscribe(EventHandler<EventArgsType> action)
+        {
+            this.OnEvent += action;
+        }
+
+        public virtual void Unsubscribe(EventHandler<EventArgsType> action)
+        {
+            this.OnEvent -= action;
+        }
+    }
+}
