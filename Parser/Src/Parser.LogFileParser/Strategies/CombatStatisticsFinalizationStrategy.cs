e7627d7c07b05f79dce11da4bbf2e41f1f3ea355
shakuu <todorov.h@gmail.com>
2017-03-06 15:03:16 +0200

    added CombatStatisticsFinalizationStrategy implementation

---
+++ Parser/Src/Parser.LogFileParser/Strategies/CombatStatisticsFinalizationStrategy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Parser.Common.Contracts;
+using Parser.LogFileParser.Contracts;
+
+namespace Parser.LogFileParser.Strategies
+{
+    public class CombatStatisticsFinalizationStrategy : ICombatStatisticsFinalizationStrategy
+    {
+        public IFinalizedCombatStatistics FinalizeCombatStatistics(ICombatStatistics combatStatistics)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
