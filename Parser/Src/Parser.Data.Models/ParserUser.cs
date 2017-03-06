680a00bb2afc868c82ad9f628d62c13970cb7f74
shakuu <todorov.h@gmail.com>
2017-03-06 20:55:38 +0200

    added ParserUser db model

--- Parser/Src/Parser.Data.Models/ParserUser.cs
+++ Parser/Src/Parser.Data.Models/ParserUser.cs
@@ -1,39 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 using Parser.Common.Constants.ErrorMessages;
 using Parser.Common.Constants.Validation;
-using Parser.Common.Contracts;
 using Parser.Data.Models.Contracts;
 
 namespace Parser.Data.Models
 {
-    public class StoredCombatStatistics : IDbModel, IFinalizedCombatStatistics
+    public class ParserUser : IDbModel
     {
+        private ICollection<StoredCombatStatistics> storedCombatStatistics;
+
+        public ParserUser()
+        {
+            this.storedCombatStatistics = new HashSet<StoredCombatStatistics>();
+        }
+
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
 
         public bool IsDeleted { get; set; }
 
-        public Guid ParserUserId { get; set; }
-
-        public virtual ParserUser ParserUser { get; set; }
-
         [Required]
         [MinLength(DbModelValidationConstants.NameMinLength, ErrorMessage = DbModelValidationErrorMessages.NameMinLengthErrorMessage)]
         [MaxLength(DbModelValidationConstants.NameMaxLength, ErrorMessage = DbModelValidationErrorMessages.NameMaxLengthErrorMessage)]
-        public string CharacterName { get; set; }
+        public string Username { get; set; }
 
-        public double CombatDurationInSeconds { get; set; }
-
-        public double DamageDone { get; set; }
-
-        public double DamageDonePerSecond { get; set; }
-
-        public double DamageTaken { get; set; }
-
-        public double DamageTakenPerSecond { get; set; }
+        public virtual ICollection<StoredCombatStatistics> StoredCombatStatistics { get { return this.storedCombatStatistics; } set { this.storedCombatStatistics = value; } }
     }
 }
