14f96f7589afb8f4a63b6ab34f7256c93ac253d1
shakuu <todorov.h@gmail.com>
2017-03-06 22:48:17 +0200

    implemented ParserDbContext

---
+++ Parser/Src/Parser.Common.Constants/Config/ConnectionStrings.cs
@@ -0,0 +1,9 @@
+﻿namespace Parser.Common.Constants.Config
+{
+    public static class ConnectionStrings
+    {
+        public const string ParserDbConnectionString = "ParserDbConnection";
+
+        public const string DefaultConnectionString = "DefaultConnection";
+    }
+}
---
+++ Parser/Src/Parser.Data/Contracts/IParserDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+using Parser.Data.Models;
+
+namespace Parser.Data.Contracts
+{
+    public interface IParserDbContext
+    {
+        IDbSet<ParserUser> ParserUsers { get; set; }
+
+        IDbSet<StoredCombatStatistics> StoredCombatStatistics { get; set; }
+
+        IDbSet<TEntity> Set<TEntity>() where TEntity : class;
+    }
+}
