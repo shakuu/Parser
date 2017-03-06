acff3b20fdbb9529241a9568e0a8c01726c90402
shakuu <todorov.h@gmail.com>
2017-03-07 01:05:37 +0200

    added ParserDataNinjectModule

---
+++ Parser/Clients/Parser.MvcClient/App_Start/NinjectModules/ParserDataNinjectModule.cs
@@ -0,0 +1,33 @@
+﻿using Ninject.Extensions.Conventions;
+using Ninject.Extensions.Conventions.Syntax;
+using Ninject.Modules;
+
+namespace Parser.MvcClient.App_Start.NinjectModules
+{
+    public class ParserDataNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Bind(this.BindAllClassesByConvention);
+            this.Bind(this.BindFactoriesByConvention);
+        }
+
+        private void BindAllClassesByConvention(IFromSyntax bind)
+        {
+            bind
+                .FromAssembliesMatching("*.Parser.Data.*")
+                .SelectAllClasses()
+                .BindDefaultInterface();
+        }
+
+        private void BindFactoriesByConvention(IFromSyntax bind)
+        {
+            bind
+                .FromAssembliesMatching("*.Parser.Data.*")
+                .SelectAllInterfaces()
+                .EndingWith("Factory")
+                .BindToFactory()
+                .Configure(f => f.InSingletonScope());
+        }
+    }
+}
