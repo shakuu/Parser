d19f74384f1d7403b536e68a6455cc7c7c2f9d2f
shakuu <todorov.h@gmail.com>
2017-03-06 20:24:57 +0200

    added StoredCombatStatistics db model

---
+++ Parser/Src/Parser.Data.Models/Contracts/IDbModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Parser.Data.Models.Contracts
+{
+    public interface IDbModel
+    {
+        Guid Id { get; set; }
+
+        bool IsDeleted { get; set; }
+    }
+}
---
+++ Parser/Src/Parser.Data.Models/StoredCombatStatistics.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Parser.Common.Contracts;
+using Parser.Data.Models.Contracts;
+
+namespace Parser.Data.Models
+{
+    public class StoredCombatStatistics : IDbModel, IFinalizedCombatStatistics
+    {
+        public Guid Id { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public string CharacterName { get; set; }
+
+        public double CombatDurationInSeconds { get; set; }
+
+        public double DamageDone { get; set; }
+
+        public double DamageDonePerSecond { get; set; }
+
+        public double DamageTaken { get; set; }
+
+        public double DamageTakenPerSecond { get; set; }
+    }
+}
