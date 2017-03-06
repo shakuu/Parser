3d4dbad213a8aadefd72ee541020c07a83a7eee9
shakuu <todorov.h@gmail.com>
2017-03-06 20:31:47 +0200

    added annotations to StoredCombatStatistics

--- Parser/Src/Parser.Data.Models/StoredCombatStatistics.cs
+++ Parser/Src/Parser.Data.Models/StoredCombatStatistics.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 using Parser.Common.Contracts;
 using Parser.Data.Models.Contracts;
@@ -7,12 +9,15 @@ namespace Parser.Data.Models
 {
     public class StoredCombatStatistics : IDbModel, IFinalizedCombatStatistics
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
 
         public bool IsDeleted { get; set; }
 
+        [Required]
         public string CharacterName { get; set; }
-
+        
         public double CombatDurationInSeconds { get; set; }
 
         public double DamageDone { get; set; }
