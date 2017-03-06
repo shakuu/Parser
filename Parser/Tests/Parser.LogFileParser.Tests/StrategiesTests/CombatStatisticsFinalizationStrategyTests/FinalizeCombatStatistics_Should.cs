4f0c127032f0e8ac4afcc046cdc136f1db90f284
shakuu <todorov.h@gmail.com>
2017-03-06 16:46:16 +0200

    added CombatStatisticsFinalizationStrategy.FinalizeCombatStatistics() tests

--- Parser/Tests/Parser.LogFileParser.Tests/StrategiesTests/CombatStatisticsFinalizationStrategyTests/FinalizeCombatStatistics_Should.cs
+++ Parser/Tests/Parser.LogFileParser.Tests/StrategiesTests/CombatStatisticsFinalizationStrategyTests/FinalizeCombatStatistics_Should.cs
@@ -31,7 +31,7 @@ namespace Parser.LogFileParser.Tests.StrategiesTests.CombatStatisticsFinalizatio
         }
 
         [TestCaseSource(typeof(FinalizeCombatStatistics_Should), "TestCases")]
-        public void ReturnIFinalizedCombatStatisticsInstance_WithCorrectData(string characterName, double damageDone, double damageTaken, string enterCombatTime, string exitCombatTime)
+        public void ReturnIFinalizedCombatStatisticsInstance_WithCorrectData(string expectedCharacterName, double expectedDamageDone, double expectedDamageTaken, string enterCombatTime, string exitCombatTime, double expectedDamageDonePerSecond, double expectedDamageTakenPerSecond, double expectedCombatDurationInSeconds)
         {
             // Arrange
             var finalizedCombatStatisticsFactory = new Mock<IFinalizedCombatStatisticsFactory>();
@@ -41,26 +41,34 @@ namespace Parser.LogFileParser.Tests.StrategiesTests.CombatStatisticsFinalizatio
             var combatStatisticsFinalizationStrategy = new CombatStatisticsFinalizationStrategy(finalizedCombatStatisticsFactory.Object);
 
             var combatStatistics = new Mock<ICombatStatistics>();
-            combatStatistics.SetupGet(s => s.CharacterName).Returns(characterName);
-            combatStatistics.SetupGet(s => s.DamageDone).Returns(damageDone);
-            combatStatistics.SetupGet(s => s.DamageTaken).Returns(damageTaken);
+            combatStatistics.SetupGet(s => s.CharacterName).Returns(expectedCharacterName);
+            combatStatistics.SetupGet(s => s.DamageDone).Returns(expectedDamageDone);
+            combatStatistics.SetupGet(s => s.DamageTaken).Returns(expectedDamageTaken);
             combatStatistics.SetupGet(s => s.EnterCombatTime).Returns(DateTime.Parse(enterCombatTime));
             combatStatistics.SetupGet(s => s.ExitCombatTime).Returns(DateTime.Parse(exitCombatTime));
 
             var actualFinalizedCommand = combatStatisticsFinalizationStrategy.FinalizeCombatStatistics(combatStatistics.Object);
 
             // Assert
-            Assert.That(actualFinalizedCommand.CharacterName, Is.EqualTo(characterName));
-            Assert.That(actualFinalizedCommand.DamageDone, Is.EqualTo(damageDone));
-            Assert.That(actualFinalizedCommand.DamageTaken, Is.EqualTo(damageTaken));
-            Assert.That(actualFinalizedCommand.CombatDurationInSeconds, Is.EqualTo(60));
+            Assert.That(actualFinalizedCommand.CharacterName, Is.EqualTo(expectedCharacterName));
+            Assert.That(actualFinalizedCommand.DamageDone, Is.EqualTo(expectedDamageDone));
+            Assert.That(actualFinalizedCommand.DamageTaken, Is.EqualTo(expectedDamageTaken));
+            Assert.That(actualFinalizedCommand.DamageDonePerSecond, Is.EqualTo(expectedDamageDonePerSecond));
+            Assert.That(actualFinalizedCommand.DamageTakenPerSecond, Is.EqualTo(expectedDamageTakenPerSecond));
+            Assert.That(actualFinalizedCommand.CombatDurationInSeconds, Is.EqualTo(expectedCombatDurationInSeconds));
         }
 
         public static IEnumerable TestCases
         {
             get
             {
-                yield return new TestCaseData("name", 1200, 1200, "22:38:00.000", "22:39:00.000");
+                // Expected parameters: 
+                // string expectedCharacterName, double expectedDamageDone, double expectedDamageTaken, string enterCombatTime, string exitCombatTime, double expectedDamageDonePerSecond, double expectedDamageTakenPerSecond, double expectedCombatDurationInSeconds
+                yield return new TestCaseData("name", 1200, 1200, "22:38:00.000", "22:39:00.000", 20, 20, 60);
+                yield return new TestCaseData("name", 1200, 1200, "22:38:00.000", "22:40:00.000", 10, 10, 120);
+                yield return new TestCaseData("another name", 1200, 1200, "22:38:00.000", "22:40:00.000", 10, 10, 120);
+                yield return new TestCaseData("another name", 2400, 2400, "22:38:00.000", "22:40:00.000", 20, 20, 120);
+                yield return new TestCaseData("another name", 2400, 2400, "22:38:00.000", "22:38:30.000", 80, 80, 30);
             }
         }
     }
