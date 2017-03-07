1c8619684ad6958098aa279ea7c5f336aa03a72a
shakuu <todorov.h@gmail.com>
2017-03-07 13:05:58 +0200

    added ObjectMapperProvider ctor tests

---
+++ Parser/Tests/Parser.Common.Tests/ProvidersTests/ObjectMapperProviderTests/Constructor_Should.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using AutoMapper;
+
+using Moq;
+using NUnit.Framework;
+
+using Parser.Common.Contracts;
+using Parser.Common.Providers;
+
+
+namespace Parser.Common.Tests.ProvidersTests.ObjectMapperProviderTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void CreateCorrectIObjectMapperProviderInstance_WhenParametersAreValid()
+        {
+            // Arrange
+            var mapper = new Mock<IMapper>();
+
+            // Act
+            var actualInstance = new ObjectMapperProvider(mapper.Object);
+
+            // Assert
+            Assert.That(actualInstance, Is.Not.Null);
+            Assert.That(actualInstance, Is.InstanceOf<IObjectMapperProvider>());
+        }
+
+        [Test]
+        public void ThrowArgumentNullException_WhenIMapperParameterIsNull()
+        {
+            // Arrange
+            IMapper mapper = null;
+
+            // Act & Assert
+            Assert.That(
+                () => new ObjectMapperProvider(mapper),
+                Throws.InstanceOf<ArgumentNullException>().With.Message.Contains(nameof(IMapper)));
+        }
+    }
+}
