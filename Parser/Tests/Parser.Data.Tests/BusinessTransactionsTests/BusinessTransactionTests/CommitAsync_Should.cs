9cbc4fda9a60c06342f6d1c8debffda23efc422e
shakuu <todorov.h@gmail.com>
2017-03-08 00:44:25 +0200

    added BusinessTransaction.CommitAsync() test

---
+++ Parser/Tests/Parser.Data.Tests/BusinessTransactionsTests/BusinessTransactionTests/CommitAsync_Should.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using NUnit.Framework;
+
+using Parser.Data.BusinessTransactions;
+using Parser.Data.Contracts;
+
+namespace Parser.Data.Tests.BusinessTransactionsTests.BusinessTransactionTests
+{
+    [TestFixture]
+    public class CommitAsync_Should
+    {
+        [Test]
+        public void InvokeIDbContext_SaveChangesAsyncMethodOnce()
+        {
+            // Arrange
+            var dbContext = new Mock<IDbContext>();
+
+            var businessTransaction = new BusinessTransaction(dbContext.Object);
+
+            // Act
+            businessTransaction.CommitAsync();
+
+            // Assert
+            dbContext.Verify(c => c.SaveChangesAsync(), Times.Once);
+        }
+    }
+}
