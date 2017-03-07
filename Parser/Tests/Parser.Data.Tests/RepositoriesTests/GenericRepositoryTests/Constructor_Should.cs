1f0e59e65931479f364af2cb1da7f697827c9c74
shakuu <todorov.h@gmail.com>
2017-03-08 00:58:13 +0200

    added GenericRepository ctor tests

---
+++ Parser/Tests/Parser.Data.Tests/RepositoriesTests/GenericRepositoryTests/Constructor_Should.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Moq;
+using NUnit.Framework;
+
+using Parser.Data.Contracts;
+using Parser.Data.Repositories;
+using Parser.Data.Tests.Mocks;
+
+namespace Parser.Data.Tests.RepositoriesTests.GenericRepositoryTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void CreateCorrectIGenericRepositoryInstance_WhenParametersAreCorrect()
+        {
+            // Arrange
+            var dbContext = new Mock<IParserDbContext>();
+
+            // Act
+            var actualInstance = new GenericRepository<MockDbModel>(dbContext.Object);
+
+            // Assert
+            Assert.That(actualInstance, Is.Not.Null);
+            Assert.That(actualInstance, Is.InstanceOf<IGenericRepository<MockDbModel>>());
+        }
+
+        [Test]
+        public void ThrowArgumentNullException_WhenIParserDbContextParameterIsNull()
+        {
+            // Arrange
+            IParserDbContext dbContext = null;
+
+            // Act & Assert
+            Assert.That(
+                () => new GenericRepository<MockDbModel>(dbContext),
+                Throws.InstanceOf<ArgumentNullException>().With.Message.Contains(nameof(IParserDbContext)));
+        }
+    }
+}
