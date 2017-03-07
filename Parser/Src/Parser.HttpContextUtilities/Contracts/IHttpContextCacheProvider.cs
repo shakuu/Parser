c628bc13da8f1db69b0f7c5e7ff569e0e243fb92
shakuu <todorov.h@gmail.com>
2017-03-07 14:45:52 +0200

    added HttpContextCacheProvider

---
+++ Parser/Src/Parser.HttpContextUtilities/Contracts/IHttpContextCacheProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace Parser.HttpContextUtilities.Contracts
+{
+    public interface IHttpContextCacheProvider
+    {
+        object this[string index] { get; }
+
+        void Add(string key, object value);
+
+        void Remove(string key);
+    }
+}
---
+++ Parser/Src/Parser.HttpContextUtilities/Providers/HttpContextCacheProvider.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+
+using Parser.HttpContextUtilities.Contracts;
+
+namespace Parser.HttpContextUtilities.Providers
+{
+    public class HttpContextCacheProvider : IHttpContextCacheProvider
+    {
+        public object this[string index]
+        {
+            get
+            {
+                return HttpContext.Current.Cache[index];
+            }
+        }
+
+        public void Add(string key, object value)
+        {
+            HttpContext.Current.Cache[key] = value;
+        }
+
+        public void Remove(string key)
+        {
+            HttpContext.Current.Cache.Remove(key);
+        }
+    }
+}
