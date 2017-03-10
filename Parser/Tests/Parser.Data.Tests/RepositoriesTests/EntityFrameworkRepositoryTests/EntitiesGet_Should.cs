ba29daf420caac67a0d8bc2fdc67b3da0e42674d
shakuu <todorov.h@gmail.com>
2017-03-10 12:22:37 +0200

    added EntityFrameworkRepository.Entities Get test

---
+++ Parser/Tests/Parser.Data.Tests/RepositoriesTests/EntityFrameworkRepositoryTests/EntitiesGet_Should.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.Entity;
+
+using Moq;
+using NUnit.Framework;
+
+using Parser.Data.Contracts;
+using Parser.Data.Repositories;
+using Parser.Data.Tests.Mocks;
+
+namespace Parser.Data.Tests.RepositoriesTests.EntityFrameworkRepositoryTests
+{
+    [TestFixture]
+    public class EntitiesGet_Should
+    {
+        [Test]
+        public void ReturnCorrectIQueryableTEntityInstance()
+        {
+            // Arrange
+            var parserDbContext = new Mock<IParserDbContext>();
+
+            var entities = new Mock<IDbSet<MockDbModel>>();
+            parserDbContext.Setup(c => c.Set<MockDbModel>()).Returns(entities.Object);
+
+            var entityFrameworkRepository = new EntityFrameworkRepository<MockDbModel>(parserDbContext.Object);
+
+            var expectedReturnerIQueryableTEntityInstance = entities.Object;
+
+            // Act
+            var actualReturnerIQueryableTEntityInstance = entityFrameworkRepository.Entities;
+
+            // Assert
+            Assert.That(actualReturnerIQueryableTEntityInstance, Is.SameAs(expectedReturnerIQueryableTEntityInstance));
+        }
+    }
+}
