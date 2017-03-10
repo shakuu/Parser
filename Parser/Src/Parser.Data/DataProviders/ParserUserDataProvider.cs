bf1c94cc92db63e5092e3528ef373500e030cca4
shakuu <todorov.h@gmail.com>
2017-03-10 22:23:54 +0200

    added ParserUserDataProvider

---
+++ Parser/Src/Parser.Data/DataProviders/ParserUserDataProvider.cs
@@ -0,0 +1,37 @@
+﻿using Bytes2you.Validation;
+
+using Parser.Common.Contracts;
+using Parser.Data.Contracts;
+using Parser.Data.Models;
+using Parser.Data.ViewModels;
+
+namespace Parser.Data.DataProviders
+{
+    public class ParserUserDataProvider : IParserUserDataProvider
+    {
+        private readonly IEntityFrameworkRepository<ParserUser> parserUserEntityFrameworkRepository;
+        private readonly IObjectMapperProvider objectMapperProvider;
+
+        public ParserUserDataProvider(IEntityFrameworkRepository<ParserUser> parserUserEntityFrameworkRepository, IObjectMapperProvider objectMapperProvider)
+        {
+            Guard.WhenArgument(parserUserEntityFrameworkRepository, nameof(IEntityFrameworkRepository<ParserUser>)).IsNull().Throw();
+            Guard.WhenArgument(objectMapperProvider, nameof(IObjectMapperProvider)).IsNull().Throw();
+
+            this.parserUserEntityFrameworkRepository = parserUserEntityFrameworkRepository;
+            this.objectMapperProvider = objectMapperProvider;
+        }
+
+        public ParserUserViewModel CreateParserUser(ParserUserViewModel model)
+        {
+            Guard.WhenArgument(model, nameof(ParserUserViewModel)).IsNull().Throw();
+
+            var parserUser = this.objectMapperProvider.Map<ParserUser>(model);
+
+            var dbParserUser = this.parserUserEntityFrameworkRepository.Create(parserUser);
+
+            var viewModel = this.objectMapperProvider.Map<ParserUserViewModel>(dbParserUser);
+
+            return viewModel;
+        }
+    }
+}
