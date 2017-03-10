b93f0d1f433f3e7053cf0bff158761712dbfce7a
shakuu <todorov.h@gmail.com>
2017-03-10 18:46:36 +0200

    implemented AuthSignInManagerProvider

---
+++ Parser/Src/Parser.Auth/Providers/AuthSignInManagerProvider.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+
+using Microsoft.AspNet.Identity.Owin;
+
+using Parser.Auth.Contracts;
+using Parser.Auth.Managers;
+
+namespace Parser.Auth.Providers
+{
+    public class AuthSignInManagerProvider : IAuthSignInManagerProvider
+    {
+        public IAuthSignInManager SignInManager
+        {
+            get
+            {
+                return HttpContext.Current.GetOwinContext().Get<AuthSignInManager>();
+            }
+        }
+    }
+}
