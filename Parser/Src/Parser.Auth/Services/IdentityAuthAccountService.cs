e7a849829e24ca75524f557590a9d4175ad2fbc9
shakuu <todorov.h@gmail.com>
2017-03-10 18:53:01 +0200

    added services to IdentityAuthAccountService ctor

--- Parser/Src/Parser.Auth/Services/IdentityAuthAccountService.cs
+++ Parser/Src/Parser.Auth/Services/IdentityAuthAccountService.cs
@@ -1,58 +1,41 @@
 ﻿using System.Threading.Tasks;
-using System.Web;
 
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 
+using Bytes2you.Validation;
+
 using Parser.Auth.Contracts;
-using Parser.Auth.Managers;
 
 namespace Parser.Auth.Services
 {
     public class IdentityAuthAccountService : IIdentityAuthAccountService
     {
-        private AuthSignInManager _signInManager;
-        private AuthUserManager _userManager;
+        private readonly IAuthSignInManagerProvider authSignInManagerProvider;
+        private readonly IAuthUserManagerProvider authUserManagerProvider;
 
-        public AuthSignInManager SignInManager
+        public IdentityAuthAccountService(IAuthSignInManagerProvider authSignInManagerProvider, IAuthUserManagerProvider authUserManagerProvider)
         {
-            get
-            {
-                // Request scope
-                return _signInManager ?? HttpContext.Current.GetOwinContext().Get<AuthSignInManager>();
-            }
-            private set
-            {
-                _signInManager = value;
-            }
-        }
+            Guard.WhenArgument(authSignInManagerProvider, nameof(IAuthSignInManagerProvider)).IsNull().Throw();
+            Guard.WhenArgument(authUserManagerProvider, nameof(IAuthUserManagerProvider)).IsNull().Throw();
 
-        public AuthUserManager UserManager
-        {
-            get
-            {
-                // Request scope
-                return _userManager ?? HttpContext.Current.GetOwinContext().GetUserManager<AuthUserManager>();
-            }
-            private set
-            {
-                _userManager = value;
-            }
+            this.authSignInManagerProvider = authSignInManagerProvider;
+            this.authUserManagerProvider = authUserManagerProvider;
         }
 
         public async Task<IdentityResult> CreateAsync(AuthUser user, string password)
         {
-            return await UserManager.CreateAsync(user, password);
+            return await this.authUserManagerProvider.UserManager.CreateAsync(user, password);
         }
 
         public async Task<SignInStatus> PasswordSignInAsync(string email, string password, bool rememberMe)
         {
-            return await this.SignInManager.PasswordSignInAsync(email, password, rememberMe, shouldLockout: false);
+            return await this.authSignInManagerProvider.SignInManager.PasswordSignInAsync(email, password, rememberMe, shouldLockout: false);
         }
 
         public async Task SignInAsync(AuthUser user)
         {
-            await this.SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+            await this.authSignInManagerProvider.SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
         }
     }
 }
