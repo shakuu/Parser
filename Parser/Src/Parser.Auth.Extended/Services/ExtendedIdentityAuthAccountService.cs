661f754a74907739d893625fef5cd955eb7e276b
shakuu <todorov.h@gmail.com>
2017-03-10 22:42:44 +0200

    added ExtendedIdentityAuthAccountService

---
+++ Parser/Src/Parser.Auth.Extended/Services/ExtendedIdentityAuthAccountService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Parser.Auth.Contracts;
+using Parser.Auth.Extended.Contracts;
+
+namespace Parser.Auth.Extended.Services
+{
+    public class ExtendedIdentityAuthAccountService : IExtendedIdentityAuthAccountService, IIdentityAuthAccountService
+    {
+        public Task<Microsoft.AspNet.Identity.IdentityResult> CreateAsync(AuthUser user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Microsoft.AspNet.Identity.Owin.SignInStatus> PasswordSignInAsync(string email, string password, bool rememberMe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SignInAsync(AuthUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
