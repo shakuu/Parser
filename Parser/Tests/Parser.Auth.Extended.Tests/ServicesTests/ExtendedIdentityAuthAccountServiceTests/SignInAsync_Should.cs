7c06b9e8418a40a487acf8ae72c07bb37ee6a195
shakuu <todorov.h@gmail.com>
2017-03-11 15:16:38 +0200

    added ExtendedIdentityAuthAccountService.SignInAsync() tests

---
+++ Parser/Tests/Parser.Auth.Extended.Tests/ServicesTests/ExtendedIdentityAuthAccountServiceTests/SignInAsync_Should.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+
+using Moq;
+using NUnit.Framework;
+
+using Parser.Auth.Contracts;
+using Parser.Auth.Extended.Services;
+using Parser.Data.Services.Contracts;
+using Parser.Data.ViewModels.Factories;
+
+namespace Parser.Auth.Extended.Tests.ServicesTests.ExtendedIdentityAuthAccountServiceTests
+{
+    [TestFixture]
+    public class SignInAsync_Should
+    {
+        [Test]
+        public async Task InvokeIIdentityAuthAccountService_SignInAsyncMethodOnceWithCorrectParameter()
+        {
+            // Arrange
+            var identityAuthAccountService = new Mock<IIdentityAuthAccountService>();
+            var registerParserUserViewModelFactory = new Mock<IRegisterParserUserViewModelFactory>();
+            var createParserUserService = new Mock<ICreateParserUserService>();
+
+            var extendedIdentityAuthAccountService = new ExtendedIdentityAuthAccountService(identityAuthAccountService.Object, registerParserUserViewModelFactory.Object, createParserUserService.Object);
+
+            var user = new AuthUser();
+
+            // Act
+            await extendedIdentityAuthAccountService.SignInAsync(user);
+
+            // Assert
+            identityAuthAccountService.Verify(s => s.SignInAsync(user), Times.Once);
+        }
+    }
+}
