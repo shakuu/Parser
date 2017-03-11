5b7359a17861a3d408cae72e83b96aff73f7e420
shakuu <todorov.h@gmail.com>
2017-03-11 23:34:30 +0200

    added CachingInterceptor

---
+++ Parser/Src/Parser.Common.Interceptors/CachingInterceptor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Bytes2you.Validation;
+
+using Ninject.Extensions.Interception;
+
+using Parser.Common.Utilities.Contracts;
+
+namespace Parser.Common.Interceptors
+{
+    public class CachingInterceptor : IInterceptor
+    {
+        private readonly IHttpContextCacheProvider httpContextCacheProvider;
+
+        public CachingInterceptor(IHttpContextCacheProvider httpContextCacheProvider)
+        {
+            Guard.WhenArgument(httpContextCacheProvider, nameof(IHttpContextCacheProvider)).IsNull().Throw();
+
+            this.httpContextCacheProvider = httpContextCacheProvider;
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
