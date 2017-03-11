7cbc1712fb0f56ecc04154c3d4d1f2250e9cea65
shakuu <todorov.h@gmail.com>
2017-03-12 00:12:35 +0200

    implemented CachingInterceptor

--- Parser/Src/Parser.Common.Interceptors/CachingInterceptor.cs
+++ Parser/Src/Parser.Common.Interceptors/CachingInterceptor.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 
 using Bytes2you.Validation;
 
 using Ninject.Extensions.Interception;
-
+using Parser.Common.Contracts;
 using Parser.Common.Utilities.Contracts;
 
 namespace Parser.Common.Interceptors
@@ -11,17 +13,63 @@ namespace Parser.Common.Interceptors
     public class CachingInterceptor : IInterceptor
     {
         private readonly IHttpContextCacheProvider httpContextCacheProvider;
+        private readonly IDateTimeProvider dateTimeProvider;
 
-        public CachingInterceptor(IHttpContextCacheProvider httpContextCacheProvider)
+        private readonly IDictionary<string, DateTime> lastCacheUpdateTimestamps;
+
+        public CachingInterceptor(IHttpContextCacheProvider httpContextCacheProvider, IDateTimeProvider dateTimeProvider)
         {
             Guard.WhenArgument(httpContextCacheProvider, nameof(IHttpContextCacheProvider)).IsNull().Throw();
+            Guard.WhenArgument(dateTimeProvider, nameof(IDateTimeProvider)).IsNull().Throw();
 
             this.httpContextCacheProvider = httpContextCacheProvider;
+            this.dateTimeProvider = dateTimeProvider;
+
+            this.lastCacheUpdateTimestamps = new ConcurrentDictionary<string, DateTime>();
         }
 
         public void Intercept(IInvocation invocation)
         {
-            throw new NotImplementedException();
+            var invokedMethodName = this.GetInvokedMethodName(invocation);
+
+            var invokedMethodTimeElapsedSincePreviousCacheUpdate = this.GetTimeElapsedSincePreviousCacheUpdate(invokedMethodName);
+            if (invokedMethodTimeElapsedSincePreviousCacheUpdate < 15)
+            {
+                invocation.ReturnValue = this.httpContextCacheProvider[invokedMethodName];
+            }
+            else
+            {
+                invocation.Proceed();
+
+                this.httpContextCacheProvider[invokedMethodName] = invocation.ReturnValue;
+                this.lastCacheUpdateTimestamps[invokedMethodName] = this.dateTimeProvider.GetUtcNow();
+            }
+        }
+
+        private string GetInvokedMethodName(IInvocation invocation)
+        {
+            var invokedMethodName = invocation.Request.Method.Name;
+
+            return invokedMethodName;
+        }
+
+        private double GetTimeElapsedSincePreviousCacheUpdate(string invokedMethodName)
+        {
+            double timeElapsed;
+
+            var invokedMethodDataIsCached = this.lastCacheUpdateTimestamps.ContainsKey(invokedMethodName);
+            if (invokedMethodDataIsCached)
+            {
+                var lastCacheUpdateTimestamp = this.lastCacheUpdateTimestamps[invokedMethodName];
+                timeElapsed = (this.dateTimeProvider.GetUtcNow() - lastCacheUpdateTimestamp).TotalMinutes;
+            }
+            else
+            {
+                this.lastCacheUpdateTimestamps.Add(invokedMethodName, this.dateTimeProvider.GetUtcNow());
+                timeElapsed = double.MaxValue;
+            }
+
+            return timeElapsed;
         }
     }
 }
