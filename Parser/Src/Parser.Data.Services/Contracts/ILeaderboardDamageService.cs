3b398e2f94913a56ee1e6542dbdb3396162f1f47
shakuu <todorov.h@gmail.com>
2017-03-15 21:55:41 +0200

    added LeaderboardDamageService

---
+++ Parser/Src/Parser.Data.Services/Contracts/ILeaderboardDamageService.cs
@@ -0,0 +1,9 @@
+﻿using Parser.Data.ViewModels.Leaderboard;
+
+namespace Parser.Data.Services.Contracts
+{
+    public interface ILeaderboardDamageService
+    {
+        DamageViewModel GetTopStoredCombatStatisticsOnPage(int pageNumber);
+    }
+}
---
+++ Parser/Src/Parser.Data.Services/LeaderboardDamageService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Parser.Data.Services.Contracts;
+using Parser.Data.ViewModels.Leaderboard;
+
+namespace Parser.Data.Services
+{
+    public class LeaderboardDamageService : ILeaderboardDamageService
+    {
+        public LeaderboardDamageService()
+        {
+
+        }
+
+        public DamageViewModel GetTopStoredCombatStatisticsOnPage(int pageNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
