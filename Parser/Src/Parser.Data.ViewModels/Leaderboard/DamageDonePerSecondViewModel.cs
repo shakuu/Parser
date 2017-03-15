7f98e6d6cf01e225bb907d5a279cbdfe3dff5895
shakuu <todorov.h@gmail.com>
2017-03-15 21:10:50 +0200

    added Damage view models

---
+++ Parser/Src/Parser.Data.ViewModels/Leaderboard/DamageDonePerSecondViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Parser.Data.ViewModels.Leaderboard
+{
+    public class DamageDonePerSecondViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string CharacterName { get; set; }
+
+        public double DamageDonePerSecond { get; set; }
+    }
+}
---
+++ Parser/Src/Parser.Data.ViewModels/Leaderboard/DamageViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Parser.Data.ViewModels.Leaderboard
+{
+    public class DamageViewModel
+    {
+        IEnumerable<DamageDonePerSecondViewModel> DamageDonePerSecondViewModels { get; set; }
+    }
+}
