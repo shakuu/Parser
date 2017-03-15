89f63840261d55ac453a735de705b3bd250a3da0
shakuu <todorov.h@gmail.com>
2017-03-15 09:50:22 +0200

    added new CacheProvider

---
+++ Parser/Src/Parser.Common.Utilities/Providers/CacheProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web;
+using System.Web.Caching;
+
+using Bytes2you.Validation;
+
+using Parser.Common.Utilities.Contracts;
+
+namespace Parser.Common.Utilities.Providers
+{
+    public class CacheProvider : ICacheProvider
+    {
+        public object this[string key]
+        {
+            get
+            {
+                return HttpContext.Current.Cache[key];
+            }
+        }
+
+        public void Add(string key, object value, DateTime absoluteExpiration)
+        {
+            Guard.WhenArgument(key, nameof(key)).IsNullOrEmpty().Throw();
+            Guard.WhenArgument(value, nameof(value)).IsNull().Throw();
+            Guard.WhenArgument(absoluteExpiration, nameof(absoluteExpiration)).IsEqual(default(DateTime)).Throw();
+
+            HttpContext.Current.Cache.Add(key, value, null, absoluteExpiration, Cache.NoSlidingExpiration, CacheItemPriority.Default, null);
+        }
+    }
+}
