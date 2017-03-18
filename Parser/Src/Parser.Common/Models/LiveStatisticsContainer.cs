319c5ababbddfb2e1da292ad8c0b0a7499805af7
shakuu <todorov.h@gmail.com>
2017-03-18 18:09:41 +0200

    added LiveStatisticsContainer

---
+++ Parser/Src/Parser.Common/Models/LiveStatisticsContainer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Parser.Common.Contracts;
+
+namespace Parser.Common.Models
+{
+    public class LiveStatisticsContainer : ILiveStatisticsContainer
+    {
+        public string CharacterName { get; set; }
+
+        public TimeSpan CombatDuration { get; set; }
+
+        public double DamageDonePerSecond { get; set; }
+
+        public double HealingDonePerSecond { get; set; }
+    }
+}
