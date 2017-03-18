82097cff855b8b415d8e60c488aa803475205978
shakuu <todorov.h@gmail.com>
2017-03-18 15:47:57 +0200

    implemented RemoteUserProvider

---
+++ Parser/Src/Parser.Auth.Remote/Factories/IRemoteUserFactory.cs
@@ -0,0 +1,9 @@
+﻿using Parser.Auth.Remote.Models;
+
+namespace Parser.Auth.Remote.Factories
+{
+    public interface IRemoteUserFactory
+    {
+        RemoteUser CreateRemoteUser(string username);
+    }
+}
