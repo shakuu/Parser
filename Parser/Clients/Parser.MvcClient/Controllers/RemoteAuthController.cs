3f79eef043828be23cfbe79bbbdab45478a39706
shakuu <todorov.h@gmail.com>
2017-03-20 00:25:39 +0200

    added RemoteAuthController

---
+++ Parser/Clients/Parser.MvcClient/Controllers/RemoteAuthController.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+
+using Microsoft.AspNet.Identity.Owin;
+
+using Parser.Auth.Contracts;
+
+namespace Parser.MvcClient.Controllers
+{
+    public class RemoteAuthController : Controller
+    {
+        private readonly IIdentityAuthAccountService identityAuthAccountService;
+
+        public RemoteAuthController(IIdentityAuthAccountService identityAuthAccountService)
+        {
+            this.identityAuthAccountService = identityAuthAccountService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(string username, string password)
+        {
+            var signInResult = await this.identityAuthAccountService.PasswordSignInAsync(username, password, false);
+            if (signInResult == SignInStatus.Success)
+            {
+                return this.Json(new { result = "success" });
+            }
+            else
+            {
+                return this.Json(new { result = string.Empty });
+            }
+        }
+    }
+}
