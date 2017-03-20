73cfbd939f1ce29786bfa807dab029828974843c
shakuu <todorov.h@gmail.com>
2017-03-20 16:04:35 +0200

    added OwnerController

---
+++ Parser/Clients/Parser.MvcClient/Controllers/OwnerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace Parser.MvcClient.Controllers
+{
+    [Authorize(Users = "myuser@user.com")]
+    public class OwnerController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
