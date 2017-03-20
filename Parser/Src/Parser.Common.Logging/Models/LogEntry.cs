6f2076f9eb0af1e8666ecca1b196e702e540bd58
shakuu <todorov.h@gmail.com>
2017-03-20 12:39:19 +0200

    added LogEntry

---
+++ Parser/Src/Parser.Common.Logging/Models/LogEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Parser.Common.Logging.Models
+{
+    public class LogEntry
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(1000)]
+        public string Message { get; set; }
+
+        [Required]
+        public MessageType MessageType { get; set; }
+    }
+}
