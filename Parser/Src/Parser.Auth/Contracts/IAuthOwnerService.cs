360fed8c48aab9510356b186edea364a33ecaba6
shakuu <todorov.h@gmail.com>
2017-03-20 23:20:17 +0200

    added GetAuthUsersOnPage method to IAuthOwnerService

--- Parser/Src/Parser.Auth/Contracts/IAuthOwnerService.cs
+++ Parser/Src/Parser.Auth/Contracts/IAuthOwnerService.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using System.Collections.Generic;
+
+using Parser.Auth.ViewModels;
 
 namespace Parser.Auth.Contracts
 {
     public interface IAuthOwnerService
     {
         void AddRoleAdmin(string username);
+
+        IEnumerable<AuthUserViewModel> GetAuthUsersOnPage(int pageNumber);
     }
 }
--- Parser/Src/Parser.Auth/Services/AuthOwnerService.cs
+++ Parser/Src/Parser.Auth/Services/AuthOwnerService.cs
@@ -1,11 +1,18 @@
-﻿using Bytes2you.Validation;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+using Bytes2you.Validation;
 
 using Parser.Auth.Contracts;
+using Parser.Auth.ViewModels;
 
 namespace Parser.Auth.Services
 {
     public class AuthOwnerService : IAuthOwnerService
     {
+        private const int DefaultPageSize = 50;
+
         private const string AdminRole = "Admin";
 
         private readonly IAuthUserManagerProvider authUserManagerProvider;
@@ -21,6 +28,15 @@ namespace Parser.Auth.Services
         {
             var user = this.authUserManagerProvider.UserManager.FindByName(username);
             this.authUserManagerProvider.UserManager.AddToRole(user.Id, AuthOwnerService.AdminRole);
+        }
+
+        public IEnumerable<AuthUserViewModel> GetAuthUsersOnPage(int pageNumber)
+        {
+            return this.authUserManagerProvider.UserManager.AuthUsers
+                .Skip(pageNumber * AuthOwnerService.DefaultPageSize)
+                .Take(AuthOwnerService.DefaultPageSize)
+                .Select(u => new AuthUserViewModel() { Username = u.UserName })
+                .ToList();
         }
     }
 }
