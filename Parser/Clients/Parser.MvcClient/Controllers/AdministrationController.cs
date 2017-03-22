e179c8b53ce2d738abae38c1dbdaed286da7fb5a
shakuu <todorov.h@gmail.com>
2017-03-22 16:31:35 +0200

    added IAdministrationService ctor param to AdministrationController

--- Parser/Clients/Parser.MvcClient/Controllers/AdministrationController.cs
+++ Parser/Clients/Parser.MvcClient/Controllers/AdministrationController.cs
@@ -1,15 +1,25 @@
 ﻿using System.Web.Mvc;
 
 using Parser.Common.Constants.Configuration;
+using Parser.Data.Services.Contracts;
 
 namespace Parser.MvcClient.Controllers
 {
     [Authorize(Roles = UserRoles.AdminRole)]
     public class AdministrationController : Controller
     {
+        private readonly IAdministrationService administrationService;
+
+        public AdministrationController(IAdministrationService administrationService)
+        {
+            this.administrationService = administrationService;
+        }
+
         [HttpGet]
         public ActionResult Index()
         {
+            var viewModel = this.administrationService.GetErrorsForPeriod(24);
+
             return View();
         }
     }
