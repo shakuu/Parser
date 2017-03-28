c87ff2877c40a38d29855d16c211f7212c164fc3
shakuu <todorov.h@gmail.com>
2017-03-28 09:22:25 +0300

    added startup time stamp to admin view for jenkins testing

--- Parser/Clients/Parser.MvcClient/App_Start/NinjectModules/CommonNinjectModule.cs
+++ Parser/Clients/Parser.MvcClient/App_Start/NinjectModules/CommonNinjectModule.cs
@@ -5,6 +5,7 @@ using Ninject.Extensions.Interception.Infrastructure.Language;
 using Ninject.Modules;
 
 using Parser.Common.Html.Svg;
+using Parser.Common.Providers;
 using Parser.Common.Utilities.Providers;
 
 namespace Parser.MvcClient.App_Start.NinjectModules
@@ -30,7 +31,8 @@ namespace Parser.MvcClient.App_Start.NinjectModules
                 .FromAssembliesMatching("*.Common.*")
                 .SelectAllClasses()
                 .BindDefaultInterface()
-                .ConfigureFor<PartialCircleSvgPathProvider>(c => c.InSingletonScope());
+                .ConfigureFor<PartialCircleSvgPathProvider>(c => c.InSingletonScope())
+                .ConfigureFor<StartupTimestampProvider>(c => c.InSingletonScope());
         }
 
         private void BindFactoriesByConvention(IFromSyntax bind)
---
+++ Parser/Src/Parser.Common/Providers/StartupTimestampProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Bytes2you.Validation;
+
+using Parser.Common.Contracts;
+
+namespace Parser.Common.Providers
+{
+    public class StartupTimestampProvider : IStartupTimestampProvider
+    {
+        public StartupTimestampProvider(IDateTimeProvider dateTimeProvider)
+        {
+            Guard.WhenArgument(dateTimeProvider, nameof(IDateTimeProvider)).IsNull().Throw();
+
+            this.LatestStartupTimestamp = dateTimeProvider.GetUtcNow();
+        }
+
+        public DateTime LatestStartupTimestamp { get; private set; }
+    }
+}
