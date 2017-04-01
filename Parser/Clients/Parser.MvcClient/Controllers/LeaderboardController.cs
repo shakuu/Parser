4d1c2394594bedadf29157c56c019b99b772d485
shakuu <todorov.h@gmail.com>
2017-04-01 16:43:39 +0300

    Refactoring LeaderboardController to use ILeaderboardService

--- Parser/Clients/Parser.MvcClient/Controllers/LeaderboardController.cs
+++ Parser/Clients/Parser.MvcClient/Controllers/LeaderboardController.cs
@@ -9,58 +9,49 @@ namespace Parser.MvcClient.Controllers
 {
     public class LeaderboardController : LoggingController
     {
-        private const int OutputCacheDurationInSeconds = 60;
+        private readonly ILeaderboardService leaderboardService;
 
-        private readonly ILeaderboardDamageService leaderboardDamageService;
-        private readonly ILeaderboardHealingService leaderboardHealingService;
-
-        public LeaderboardController(ILeaderboardDamageService leaderboardDamageService, ILeaderboardHealingService leaderboardHealingService)
+        public LeaderboardController(ILeaderboardService leaderboardService)
         {
-            Guard.WhenArgument(leaderboardDamageService, nameof(ILeaderboardDamageService)).IsNull().Throw();
-            Guard.WhenArgument(leaderboardHealingService, nameof(ILeaderboardHealingService)).IsNull().Throw();
-
-            this.leaderboardDamageService = leaderboardDamageService;
-            this.leaderboardHealingService = leaderboardHealingService;
+            Guard.WhenArgument(leaderboardService, nameof(ILeaderboardService)).IsNull().Throw();
+            
+            this.leaderboardService = leaderboardService;
         }
 
         [HttpGet]
-        //[OutputCache(Duration = LeaderboardController.OutputCacheDurationInSeconds, VaryByParam = "none", Location = System.Web.UI.OutputCacheLocation.Any)]
         public ActionResult Damage()
         {
-            var viewModel = this.leaderboardDamageService.GetTopStoredDamageOnPage(0);
+            var viewModel = this.leaderboardService.GetTopDamageOnPage(0);
 
             return this.View(viewModel);
         }
 
         [HttpPost]
-        //[OutputCache(Duration = LeaderboardController.OutputCacheDurationInSeconds, VaryByParam = "pageNumber", Location = System.Web.UI.OutputCacheLocation.Any)]
         [ValidateAntiForgeryToken]
         public ActionResult Damage(int? pageNumber)
         {
             pageNumber = this.ValidatePageNumber(pageNumber);
 
-            var viewModel = this.leaderboardDamageService.GetTopStoredDamageOnPage(pageNumber.Value + 1);
+            var viewModel = this.leaderboardService.GetTopDamageOnPage(pageNumber.Value + 1);
 
             return this.PartialView("_DamageDonePerSecondViewModelsPartial", viewModel);
         }
 
         [HttpGet]
-        //[OutputCache(Duration = LeaderboardController.OutputCacheDurationInSeconds, VaryByParam = "none", Location = System.Web.UI.OutputCacheLocation.Any)]
         public ActionResult Healing()
         {
-            var viewModel = this.leaderboardHealingService.GetTopStoredHealingOnPage(0);
+            var viewModel = this.leaderboardService.GetTopHealingOnPage(0);
 
             return this.View(viewModel);
         }
 
         [HttpPost]
-        //[OutputCache(Duration = LeaderboardController.OutputCacheDurationInSeconds, VaryByParam = "pageNumber", Location = System.Web.UI.OutputCacheLocation.Any)]
         [ValidateAntiForgeryToken]
         public ActionResult Healing(int? pageNumber)
         {
             pageNumber = this.ValidatePageNumber(pageNumber);
 
-            var viewModel = this.leaderboardHealingService.GetTopStoredHealingOnPage(pageNumber.Value + 1);
+            var viewModel = this.leaderboardService.GetTopHealingOnPage(pageNumber.Value + 1);
 
             return this.PartialView("_HealingDonePerSecondViewModelsPartial", viewModel);
         }
