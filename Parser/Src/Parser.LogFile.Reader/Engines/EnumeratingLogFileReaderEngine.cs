635497e997c8a2919961d75079dfbec8628092fd
shakuu <todorov.h@gmail.com>
2017-04-06 13:04:49 +0300

    Added EnumeratingLogFileReaderEngine
    - Overriding AwaitLogFileChanges

---
+++ Parser/Src/Parser.LogFile.Reader/Engines/EnumeratingLogFileReaderEngine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Parser.LogFile.Reader.Contracts;
+using Parser.LogFile.Reader.Factories;
+
+namespace Parser.LogFile.Reader.Engines
+{
+    public class EnumeratingLogFileReaderEngine : LogFileReaderEngine, ILogFileReaderEngine, IDisposable
+    {
+        private readonly IForceCommandUtilizationStrategy forceCommandUtilizationStrategy;
+
+        public EnumeratingLogFileReaderEngine(ICommandParsingStrategy commandParsingStrategy, IForceCommandUtilizationStrategy forceCommandUtilizationStrategy, ILogFilePathDiscoveryStrategy logFilePathDiscoveryStrategy, IFileReaderAutoResetEventFactory fileReaderAutoResetEventFactory, IFileReaderFileSystemWatcherFactory fileReaderFileSystemWatcherFactory, IFileReaderInputProviderFactory fileReaderInputProviderFactory)
+            : base(commandParsingStrategy, forceCommandUtilizationStrategy, logFilePathDiscoveryStrategy, fileReaderAutoResetEventFactory, fileReaderFileSystemWatcherFactory, fileReaderInputProviderFactory)
+        {
+            this.forceCommandUtilizationStrategy = forceCommandUtilizationStrategy;
+        }
+
+        protected override void AwaitLogFileChanges()
+        {
+            this.forceCommandUtilizationStrategy.ForceUtilizeCommand();
+
+            base.AwaitLogFileChanges();
+        }
+    }
+}
